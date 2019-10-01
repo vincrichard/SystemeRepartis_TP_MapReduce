b850d67fcf02b8b610cbbda9c26e596ecb422380
vincrichard <vincent.richard1094@gmail.com>
2019-10-01 09:16:58 +0200

    Etape 6.1

--- SLAVE/src/Slave/Slave.java
+++ SLAVE/src/Slave/Slave.java
@@ -2,6 +2,11 @@ package Slave;
 
 public class Slave {
     public static void main (String[] args){
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         int a = 3+5;
         System.out.println(a);
     }
