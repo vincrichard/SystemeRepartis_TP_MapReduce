e2e4842f7dddcd1373f9d7277da08de417b1d8f7
vincrichard <vincent.richard1094@gmail.com>
2019-10-14 14:41:04 +0200

    Fin Etape 7.2

---
+++ DEPLOY/src/ConnectingThread.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class ConnectingThread extends Thread{
+    private ProcessBuilder processBuilder;
+    private Process process;
+
+    public ConnectingThread(ProcessBuilder processBuilder) {
+        this.processBuilder = processBuilder;
+        this.process = null;
+        this.start();
+//        p.waitFor();
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        try {
+            process = processBuilder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        LinkedBlockingQueue<String> errorQueue = new LinkedBlockingQueue<>();
+        LinkedBlockingQueue<String> inputQueue = new LinkedBlockingQueue<>();
+        Thread readError = new Publisher(process, errorQueue, Boolean.TRUE);
+        Thread readInput = new Publisher(process, inputQueue);
+        Thread displayError = new ConsumerError(errorQueue);
+        Thread displayInput = new ConsumerInput(process, inputQueue);
+    }
+
+    public Process getProcess() {
+        return process;
+    }
+}
--- DEPLOY/src/DEPLOY.java
+++ DEPLOY/src/DEPLOY.java
@@ -1,21 +1,36 @@
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.stream.Collectors;
 
 public class DEPLOY {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        List<String> ipList = Files.readAllLines(Paths.get("ipAdress"));
-//        ProcessBuilder pb = new ProcessBuilder("ssh", "vrichard@"+"c130-16", "hostname");
-//        ConnectingProcess connectingProcess = new ConnectingProcess(pb);
+        List<String> ipList = Files.readAllLines(Paths.get("../data/ipAdress"));
+        List<Thread> threadList = new ArrayList<>();
+        //Process de la creation du dossier /tmp/vrichard/
         for (String ip: ipList) {
-            ProcessBuilder pb = new ProcessBuilder("ssh", "vrichard@"+ip, "hostname");
-            ConnectingProcess connectingProcess = new ConnectingProcess(pb);
+            ProcessBuilder pbMkdir = new ProcessBuilder("ssh", "vrichard@" + ip, "mkdir -p /tmp/vrichard/");
+            ConnectingThread mkdirThread = new ConnectingThread(pbMkdir);
+            threadList.add(mkdirThread);
         }
-
+        //Attente de la fin de tous les process de création des dossiers
+        for (Thread thread: threadList) {
+            thread.join();
+        }
+        //Process de copie des fichiers .jar
+        for (String ip: ipList){
+            ProcessBuilder pbScpJar = new ProcessBuilder("scp",
+                    "/media/vincent/C0FC3B20FC3B0FE0/MSBGD/SystemeRepartie/TPMapReduce/SLAVE/out/artifacts/SLAVE_jar/SLAVE.jar",
+                    "vrichard@"+ip+":/tmp/vrichard/");
+            ConnectingThread scpThread = new ConnectingThread(pbScpJar);
+            threadList.add(scpThread);
+        }
+        //Attente de la fin des copie des fichier .jar
+        for (Thread thread: threadList) {
+            thread.join();
+        }
     }
 }
+
