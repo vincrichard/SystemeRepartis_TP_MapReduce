35e04f3edb3cc14e2fedfb32563089ed781121a0
vincrichard <vincent.richard1094@gmail.com>
2019-10-14 17:38:02 +0200

    Etape 10.1

--- MASTER/src/Master.java
+++ MASTER/src/Master.java
@@ -9,6 +9,11 @@ import java.util.stream.Collectors;
 public class Master {
 
     public static void  main(String[] args) throws IOException, InterruptedException {
+//        runSlave();
+        copySlpits();
+    }
+
+    private static void runSlave() throws IOException, InterruptedException {
         List<String> ipList = Files.readAllLines(Paths.get("/media/vincent/C0FC3B20FC3B0FE0/MSBGD/SystemeRepartie/TPMapReduce/data/ipAdress"));
         List<Thread> threadList = new ArrayList<>();
         //Process run le programme SLAVE sur les machines connecté en ssh
@@ -23,5 +28,35 @@ public class Master {
         }
     }
 
-
+    private static void copySlpits() throws IOException, InterruptedException {
+        List<String> ipList = Files.readAllLines(Paths.get("/media/vincent/C0FC3B20FC3B0FE0/MSBGD/SystemeRepartie/TPMapReduce/data/ipAdress"));
+        List<Thread> threadList = new ArrayList<>();
+        //Process run le programme SLAVE sur les machines connecté en ssh
+        for (String ip: ipList) {
+            ProcessBuilder pbMkdir = new ProcessBuilder("ssh", "vrichard@" + ip, "mkdir -p /tmp/vrichard/splits/");
+            ConnectingThread mkdirThread = new ConnectingThread(pbMkdir);
+            threadList.add(mkdirThread);
+        }
+        //Attente de la fin de tous les process de création des dossiers
+        for (Thread thread: threadList) {
+            thread.join();
+        }
+        //Recupération des splits
+        File splitsDirectory = new File("/media/vincent/C0FC3B20FC3B0FE0/MSBGD/SystemeRepartie/TPMapReduce/data/splits/");
+        String [] splitsList = splitsDirectory.list();
+        //Process de copie les splits
+        for (String ip: ipList){
+            for (String split: splitsList) {
+                ProcessBuilder pbScpSplit = new ProcessBuilder("scp",
+                        "/media/vincent/C0FC3B20FC3B0FE0/MSBGD/SystemeRepartie/TPMapReduce/data/splits/"+split,
+                        "vrichard@" + ip + ":/tmp/vrichard/splits/"+split);
+                ConnectingThread scpThread = new ConnectingThread(pbScpSplit);
+                threadList.add(scpThread);
+            }
+        }
+        //Attente de la fin de tous les process
+        for (Thread thread: threadList) {
+            thread.join();
+        }
+    }
 }
